494c838f8e61f69f0a8fff8798c1377a9efc166c
Mirshod0921 <mirshodmuhammedov3@gmail.com>
2025-03-23 21:31:12 +0500

    homework

---
+++ lesson-14/homework/SQLQuery1.sql
@@ -0,0 +1,27 @@
+use class14
+
+declare @result varchar(max) = 
+(
+    select
+        tbs.name as td, '',
+        ids.name as td, '',
+        ids.type_desc as td, '',
+        ty.name as td
+    from sys.indexes ids
+    left join sys.tables tbs
+        on ids.object_id = tbs.object_id
+    left join sys.columns co
+        on co.object_id = ids.object_id
+    left join sys.types ty
+        on ty.system_type_id = co.system_type_id
+    for xml path('tr')
+)
+
+declare @html_body varchar(max) = '<table>' + @result + '</table>';
+
+EXEC msdb.dbo.sp_send_dbmail
+    @profile_name = 'Gmail profile',
+    @recipients = 'mirshodmuhammedov3@gmail.com',
+    @subject = 'Metadata about indeces',
+    @body = @html_body,
+    @body_format = 'HTML';
